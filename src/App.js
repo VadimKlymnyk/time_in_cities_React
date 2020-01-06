c7bf5a49093adfe141b54139e8bc1b8e87ed03e9
VadimKlymnyk <klumnukv@gmail.com>
2020-01-06 22:17:25 +0200

    change

--- src/App.js
+++ src/App.js
@@ -11,23 +11,23 @@ function App() {
   let [t, setT] = React.useState(new Date());
 
 
-   t.setTime(t.getTime() + 1000);
+  t.setTime(t.getTime() + 1000);
 
 
-   window.addEventListener('load', function () {
+  window.addEventListener('load', function () {
 
-    if(localStorage.getItem('time') !== 'null' )  {
-      let in_time =localStorage.getItem('time');
+    if (localStorage.getItem('time') !== 'null') {
+      let in_time = localStorage.getItem('time');
       t.setHours(+in_time.substr(0, 2))
       t.setMinutes(+in_time.substr(3, 2));
       t.setSeconds(+in_time.substr(6, 2));
-      document.querySelector('input[type="time"]').value=in_time
+      document.querySelector('input[type="time"]').value = in_time
     }
-    
-      
+
+
   });
-    
-  
+
+
 
 
 
@@ -38,61 +38,65 @@ function App() {
         <div className="header__title">
           City time
         </div>
-
-        <div className="section">
-          <div className="time">
-            <div className="box">
-              <div className="box__time1">
-                 <span className="time_">{t.getUTCHours()}:{t.getMinutes()}:{t.getSeconds()}</span>
-              </div>
-              <div className="box__title">London</div>
+      </div>
+      <div className="section">
+        <div className="time">
+          <div className="box">
+            <div className="box__time1">
+              <span className="time_">{t.getUTCHours()}:{t.getMinutes()}:{t.getSeconds()}</span>
             </div>
-            <div className="box">
-              <div className="box__time2"></div>
-              <span className="time_">{t.toLocaleTimeString()}</span>
-              <div className="box__title">Kiev</div>
-            </div>
-            <div className="box">
-              <div className="box__time3"></div>
-              <span className="time_">{t.getHours()-1}:{t.getMinutes()}:{t.getSeconds()}</span>
-              <div className="box__title">Rome</div>
-            </div>
+            <div className="box__title">London</div>
           </div>
-          <div className="deb">
-            <div className="deb_title">Select time</div>
-            <input
-              id="deb_time"
-              type="time"
-              name="in_time"
-              step="1"
-              onInput={() =>
-                In_time()
-              }
-              
-            />
-            <button 
-            className="deb_btn" 
-            onClick={() => 
-            
+          <div className="box">
+            <div className="box__time2"></div>
+            <span className="time_">{t.toLocaleTimeString()}</span>
+            <div className="box__title">Kiev</div>
+          </div>
+          <div className="box">
+            <div className="box__time3"></div>
+            <span className="time_">{t.getHours() - 1}:{t.getMinutes()}:{t.getSeconds()}</span>
+            <div className="box__title">Rome</div>
+          </div>
+        </div>
+        <div className="deb">
+          <div className="deb_title">Select time</div>
+          <input
+            id="deb_time"
+            type="time"
+            name="in_time"
+            step="1"
+            onInput={() =>
+              In_time()
+            }
+
+          />
+          <button
+            className="deb_btn"
+            onClick={() =>
+
               Btn_click()
             }>Current time</button>
-          </div>
-
         </div>
+
+
+
       </div>
+      <div className="etc">
+        Vadym Klymniuk
+        </div>
     </div>
 
   );
 
-  function Btn_click(){
-    document.querySelector('input[type="time"]').value=null
+  function Btn_click() {
+    document.querySelector('input[type="time"]').value = null
     localStorage.setItem('time', null);
-    return(
+    return (
       setT(new Date())
     )
   }
 
-  
+
 
   function In_time() {
     let input_time = document.querySelector('input[type="time"]').value;
